a919e0210ef633f994264eac805f40365c865307
wahabu <ritscg2hibo@gmail.com>
2023-05-16 20:44:14 +0900

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,30 +1,43 @@
 const quiz = [
   {
-    question: '先生は学生に宿題を________。',
-    answers: ['します', 'くれます', 'あげます', 'もらいます'],
-    correctAnswer: 'あげます'
+      question: '先生は学生に宿題を________。',
+      answers: ['します', 'くれます', 'あげます', 'もらいます'],
+      correctAnswer: 'あげます'
   },
   {
-    question: 'つくえ',
-    answers: ['机', '電話', '車', '服'],
-    correctAnswer: '机'
+      question: 'つくえ',
+      answers: ['机', '電話', '車', '服'],
+      correctAnswer: '机'
   },
   {
-    question: '明日',
-    answers: ['あした', 'あさって', 'きょう', 'いま'],
-    correctAnswer: 'あした'
+      question: '明日',
+      answers: ['あした', 'あさって', 'きょう', 'いま'],
+      correctAnswer: 'あした'
   }
 ];
 
-function startQuiz() {
-  for(let i = 0; i < quiz.length; i++) {
-    const answer = prompt(quiz[i].question + '\n' + quiz[i].answers.join('\n'));
-    if(answer === quiz[i].correctAnswer) {
-      alert('Correct!');
-    } else {
-      alert('Incorrect. The correct answer was ' + quiz[i].correctAnswer);
-    }
+let currentQuestion = 0;
+
+function showQuestion() {
+  const questionItem = quiz[currentQuestion];
+  document.getElementById('question').innerText = questionItem.question;
+  for(let i = 1; i <= 4; i++) {
+      const button = document.getElementById('answer' + i);
+      button.innerText = questionItem.answers[i - 1];
+      button.onclick = function() {
+          if(button.innerText === questionItem.correctAnswer) {
+              alert('Correct!');
+          } else {
+              alert('Incorrect. The correct answer was ' + questionItem.correctAnswer);
+          }
+          currentQuestion++;
+          if(currentQuestion < quiz.length) {
+              showQuestion();
+          } else {
+              alert('Quiz completed!');
+          }
+      };
   }
 }
 
-startQuiz();
+showQuestion();
